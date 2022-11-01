68a3d6cc87f6c761f170391df45db7e1025afdbf
chandansen <chandansenbu@gmail.com>
2022-11-01 19:26:27 +0600

    udate design

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 
+//Navbar code
+// Header Scroll
+
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+} 
+
+
+
+
+
+
+
+
 // benifits tab switcher 
 
 const tab_Switchers = document.querySelectorAll("[data-switcher]");
