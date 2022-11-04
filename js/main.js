0945de0c16662262674520328b6e57af8c163430
chandansen <chandansenbu@gmail.com>
2022-11-04 14:47:41 +0600

    add register page

--- js/main.js
+++ js/main.js
@@ -100,3 +100,27 @@ $(document).ready(function () {
 });
 
 
+
+
+
+
+// form custom select option
+
+// custom cetagory selecct option 
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+  o.addEventListener("click", () => {
+    selected.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+  });
+});
+
+
